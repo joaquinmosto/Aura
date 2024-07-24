987a082e27612f8db2c0eda9ab9132e2544a7b2d
jmosto <jmosto@paseshow.com.ar>
2024-07-24 11:14:28 -0300

    agrego las dos entidades y queda todo configurado para poder mapearlas en base

---
+++ src/Entity/Rol.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RolRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RolRepository::class)]
+class Rol
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Strategy = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): static
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    public function getStrategy(): ?string
+    {
+        return $this->Strategy;
+    }
+
+    public function setStrategy(string $Strategy): static
+    {
+        $this->Strategy = $Strategy;
+
+        return $this;
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $UserName = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Password = null;
+
+    #[ORM\OneToOne(targetEntity: Rol::class)]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Rol $rol = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUserName(): ?string
+    {
+        return $this->UserName;
+    }
+
+    public function setUserName(string $UserName): static
+    {
+        $this->UserName = $UserName;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->Password;
+    }
+
+    public function setPassword(string $Password): static
+    {
+        $this->Password = $Password;
+
+        return $this;
+    }
+
+    public function getRolId(): ?int
+    {
+        return $this->rol;
+    }
+
+    public function setRolId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+
+}
