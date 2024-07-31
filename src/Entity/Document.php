a4e1727fab8172e9c65fa3f89964e278f4ef6bfc
jmosto <jmosto@paseshow.com.ar>
2024-07-31 16:08:01 -0300

    agrego entidades de documentos

---
+++ src/Entity/Document.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\DocumentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: DocumentRepository::class)]
+class Document
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $code = null;
+
+    #[ORM\ManyToOne]
+    private ?DocumentType $type = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(string $code): static
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getType(): ?DocumentType
+    {
+        return $this->type;
+    }
+
+    public function setType(?DocumentType $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+}
